cdfc1b23fad8503aed96b78555e596554b6b7d38
Juan Donquis <juandiegodonquisdiaz@gmail.com>
2022-06-14 14:07:42 -0400

    create sections navbar and student menu for our template

--- template/sections/_student-menu.php
+++ template/sections/_student-menu.php
@@ -1,5 +1,4 @@
  <nav class="mt-2">
-
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-header">TASKS</li>
           <!-- Add icons to the links using the .nav-icon class
@@ -8,13 +7,10 @@
             <a href="request.php" class="nav-link">
               <i class="nav-icon far fa-calendar-alt"></i>
               <p>
-                Solicitudes 
+                Nada xd
                 <span class="badge badge-info right">2</span>
               </p>
             </a>
           </li>
  </ul>
       </nav>
-
-
-    
