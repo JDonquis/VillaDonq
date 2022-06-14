45d8dcf3e859ff75ccc289ff3096f538aeaf1d56
Juan Donquis <juandiegodonquisdiaz@gmail.com>
2022-06-14 11:55:54 -0400

    Add file template

---
+++ template/sections/_modal-request.php
@@ -0,0 +1,171 @@
+
+
+<div class="request-modal">
+  
+  <div class="request-modal-content rounded">
+
+  
+
+     <div class="row pt-3">
+
+      <div class="ribbon-wrapper ribbon-lg">
+        <div class="ribbon bg-info" id="status">
+          
+        </div>
+       </div>
+
+        <div class="col-3">
+          <div class="box-img ml-5 ">
+            <img src="" alt="Photo" class="photo_request rounded-circle">
+          </div>
+        </div>
+
+          <div class="col-6 text-center">
+            <div>
+              <h1 class="title-request text-primary">Solicitudes</h1>
+              <p>Informacion de solicitud</p>
+            </div>
+        </div>
+
+            <div class="col-3 d-flex flex-column" id="card-id-status">
+
+              <div class="d-flex flex-row">
+                <label>ID: </label><p id="id_request">16</p>
+              </div>
+
+              <button class="btn btn-primary rounded-circle w-50" id="exit-modal">Salir</button>   
+              
+        </div>
+
+
+
+     </div>
+      
+      <div class="row justify-content-around mt-5">
+          <div class="box-request col-3 d-inline-flex">
+            
+            <label>Nombre: </label><p class="pl-2" id="name_request">Null</p>
+
+          </div>
+
+            <div class="box-request col-3 d-inline-flex ">
+            
+             <label>Apellido: </label><p class="pl-2" id="last_name_request">Null</p>
+
+          </div>
+
+             <div class="box-request col-3 d-inline-flex ">
+            
+             <label>DNI: </label><p class="pl-2" id="DNI_request">Null</p>
+
+          </div>
+      </div>
+
+
+    <div class="row justify-content-around margin-top-edit">
+
+          <div class="box-request col-3 d-inline-flex">
+            
+            <label>Fecha de nacimiento: </label><p class="pl-2" id="date_b_request">Null</p>
+
+          </div>
+
+            <div class="box-request col-3 d-inline-flex ">
+            
+             <label>Edad: </label><p class="pl-2" id="age_request">Null</p>
+
+          </div>
+
+             <div class="box-request col-3 d-inline-flex ">
+            
+             <label>Correo: </label><p class="pl-2" id="email_request">Null</p>
+
+          </div>
+
+      
+      </div>
+
+      <div class="row justify-content-around margin-top-edit">
+
+          <div class="box-request col-3 d-inline-flex align-self-start ">
+            
+             <label>Numero de telefono: </label><p class="pl-2" id="phone_request">Null</p>
+
+          </div>
+
+          <div class="box-request col-3 align-self-start">
+            
+            <label>Direccion: </label><p class="pl-2" id="address_request">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime voluptate aliquid, deserunt voluptatum. Eaque velit praesentium iure excepturi ducimus laudantium expedita corporis, veritatis doloribus, alias recusandae, a dolore adipisci placeat.</p>
+
+          </div>
+
+             <div class="box-request col-3 align-self-start">
+            
+              <label>Boleta: </label><a href="#" target="_blank" class="pl-2" id="report_card_request">Ver Documento</a>
+              <!-- <embed src="../request_images/photo/V30462117-1-IMG_20200124_123116.jpg" type="image/jpg" class="document"> -->
+              
+
+             </div>
+
+          
+       
+       </div>
+
+      <div class="row justify-content-around margin-top-edit">
+
+          <div class="box-request col-3 align-self-start">
+            
+              <label>Notas Certificadas: </label><a href="#" target="_blank" class="pl-2" id="certificate_notes">Ver Documento</a>
+              <!-- <embed src="../request_images/certificate_notes/V30462117-1-IVAN.pdf" type="application/pdf" class="document"> -->
+              
+
+         </div>
+        
+         <div class="box-request col-3 align-self-start">
+            
+              <label>C.Buena conducta: </label><a href="#" target="_blank" class="pl-2" id="certificate_conduct">Ver Documento</a>
+              <!-- <embed src="../request_images/photo/V30462117-1-IMG_20200124_123116.jpg" type="image/jpg" class="document"> -->
+              
+
+          </div>
+
+          <div class="box-request col-3 align-self-start">
+            
+              <label>Acta de Nacimiento: </label><a href="" target="_blank" class="pl-2" id="birth_certificate">Ver Documento</a>
+              <!-- <embed src="../request_images/photo/V30462117-1-IMG_20200124_123116.jpg" type="image/jpg" class="document"> -->
+              
+
+          </div>
+
+    
+
+            
+       
+       </div>
+
+       <div class="row justify-content-around margin-top-edit">
+
+                <div class="box-request col-3 d-inline-flex ">    
+                   <label>DNI Representante: </label><p class="pl-2" id="DNI_repre">30847627</p>
+                </div>
+
+                  <div class="box-request col-3 d-inline-flex ">    
+                   <label>N.C Representante: </label><p class="pl-2" id="fullname_repre">Deivis Donquis</p>
+                </div>
+
+                  <div class="box-request col-3 d-inline-flex ">    
+                   <label>Telefono Representante: </label><p class="pl-2" id="phone_repre">04125800610</p>
+                </div>
+
+       </div>
+
+  </div>
+
+</div>
+
+
+
+
+
+
+
