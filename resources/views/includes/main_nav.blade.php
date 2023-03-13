0a91e16060e5b4fc1d3ba2a544701808fd9c36b3
juandonquis <juandonquis07@gmail.com>
2023-03-12 21:08:09 -0400

    Add hamburger button and link

--- resources/views/includes/main_nav.blade.php
+++ resources/views/includes/main_nav.blade.php
@@ -13,13 +13,19 @@
             <div class="nav-section">
                 @if(!session()->has('id_user'))
                 <button class="entrar-btn trigger_modal" data-modal="login_form">Entrar</button>
-                <button class="trigger_nav trigger_modal" data-modal="nav_content">
+{{--                 <button class="trigger_nav trigger_modal" data-modal="nav_content">
                     <svg width="34" height="26" viewBox="0 0 34 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path class='path-1' d="M0.5 2C10.9 2 27.1667 2 34 2" stroke-width="3" />
                         <path class='path-2' d="M0 13C10.4 13 26.6667 13 33.5 13" stroke-width="3" />
                         <path class='path-3' d="M0 24C10.4 24 26.6667 24 33.5 24" stroke-width="3" />
                     </svg>
+                </button> --}}
+                <button class="hamburger hamburger--collapse trigger_nav trigger_modal" type="button" data-modal="nav_content">
+                  <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                  </span>
                 </button>
+
                 @else
                 <a href="{{route('workspace')}}">Workspace</a>
                 @endif
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset("assets/hamburger_css/hamburger.css")}}">
     <link rel="stylesheet" href="{{asset("assets/css/welcome.css")}}">
     
     <title>VillaDonq</title>
 </head>  
+
 
 <body>
 
